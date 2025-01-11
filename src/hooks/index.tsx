568a8fd409f60d5512fcd74d0625dd78567e1d7b
Mauro-Filho <michelgoiania2020@gmail.com>
2025-01-11 14:33:05 -0300

    feat: integraçao backoffice

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { AuthBackofficeProvider } from "./authBackoffice";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const Hooks: React.FC = ({ children }) => {
+// Aqui, PropsWithChildren define que o componente espera a propriedade children
+export const Hooks: React.FC<{ children: ReactNode }> = ({ children }) => {
   return (
-      <AuthBackofficeProvider>
-        <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>
-      </AuthBackofficeProvider>
+    <AuthBackofficeProvider>
+      <QueryClientProvider client={new QueryClient()}>{children}</QueryClientProvider>
+    </AuthBackofficeProvider>
   );
 };
